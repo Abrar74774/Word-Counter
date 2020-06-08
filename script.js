72d6a8dcb9a73e70756a417f83ab036296b42201
Abrar74774 <abrarshahriarhossain@gmail.com>
2020-06-08 17:41:59 +0300

    redesigned

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ var output = document.getElementById("output");
 
 function splitWords() {
   var input = String(document.getElementById("input").value);
-  
-  words =  input.split(/\s+/);  
+
+  words = input.split(/\s+/);
   if (words[0] == " ") {
     words.shift();
   }
   if (words[words.length - 1] == " ") {
     words.pop();
-  } 
+  }
   console.log(words);
-  output.innerHTML = words.length - 1; 
+  output.innerHTML = words.length - 1;
 }
- 
-txt.addEventListener("input", splitWords);
-txt.addEventListener("focus", function(){txt.style.boxShadow = "0px 0px 4px 4px dodgerblue"});
-txt.addEventListener("blur", function(){txt.style.boxShadow = "0 0 0 0 dodgerblue"});
+
+txt.addEventListener("keyup", splitWords);
+txt.addEventListener("focus", function () { txt.style.boxShadow = "0px 0px 8px 2px #00000024" });
+txt.addEventListener("blur", function () { txt.style.boxShadow = "none" });
 
